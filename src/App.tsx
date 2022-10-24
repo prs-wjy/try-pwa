ede66449825a0cfea371c8a146f5002eab5b7b1d
prs-wjy <prs.wjy@gmail.com>
2022-10-24 21:21:17 +0800

    iframe and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,45 @@
 import "./App.css";
 
-import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button
-          className="App-button"
-          onClick={() => (document.location = "https://duadigital.com")}
-        >
-          OPEN WEB
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="iframe" element={<Iframe />} />
+      </Routes>
     </div>
+  );
+}
+function Home() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a className="App-button" href="/iframe">
+        OPEN WEB
+      </a>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+    </header>
+  );
+}
+
+function Iframe() {
+  return (
+    <iframe
+      src="https://duadigital.com"
+      frameBorder={0}
+      style={{ position: "absolute", left: 0, height: "100%", width: "100%" }}
+      height="100%"
+      width="100%"
+    />
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -10,7 +11,9 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
