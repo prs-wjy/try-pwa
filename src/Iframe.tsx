53e9914560d6cf0f641df1761ac56c1dc4b6d8f6
prs-wjy <prs.wjy@gmail.com>
2022-10-25 14:17:46 +0800

    loading and link

--- src/Iframe.tsx
+++ src/Iframe.tsx
@@ -1,12 +1,43 @@
+import { useState } from "react";
+
 function Iframe() {
+  const [isIframeLoaded, setIsIframeLoaded] = useState(false);
+
   return (
-    <iframe
-      src="http://192.168.227.91:8123"
-      frameBorder={0}
-      style={{ position: "absolute", left: 0, height: "100vh", width: "100%" }}
-      height="100%"
-      width="100%"
-    />
+    <div style={{ height: "100vh", width: "100%" }}>
+      {!isIframeLoaded && (
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            height: "100%",
+            width: "100%",
+            zIndex: 10,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <img src="loading.svg" alt="loading" width={100} />
+        </div>
+      )}
+      <iframe
+        title="web"
+        src="https://developers.home-assistant.io/"
+        frameBorder={0}
+        style={{
+          position: "absolute",
+          left: 0,
+          height: "100%",
+          width: "100%",
+          visibility: isIframeLoaded ? "visible" : "hidden",
+        }}
+        height="100%"
+        width="100%"
+        onLoad={() => setIsIframeLoaded(true)}
+      />
+    </div>
   );
 }
 
